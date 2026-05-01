82992aea0a48442b555520be2bc8111611a2d95b
Alice P. <alicepav231@gmail.com>
2026-05-01 15:17:24 +0200

    Change printf format strings and outputs

--- main.c
+++ main.c
@@ -20,13 +20,12 @@ int	main(void)
 	int	b;
 
 	x = 42;
-	a = ft_printf("Mine: %c %s %p %d %i %u %x %X %%\n",
+	a = ft_printf("Fake: %c %s %p %d %i %u %x %X %%\n",
 			'A', "hello", &x, -42, 42, 3000000000u, 255, 255);
 	b = printf("Real: %c %s %p %d %i %u %x %X %%\n",
 			'A', "hello", &x, -42, 42, 3000000000u, 255, 255);
-	printf("ft_printf return: %d\n", a);
-	printf("printf return: %d\n", b);
-	printf("NULL format return: %d\n", ft_printf("%d\nuijnmji9ujuojouijioujoiuojiujiuojiujiujoiji;ojijijioio;j;joijoijoip;pjij;oij;oijoioji;ojioij;ojiojij;iojjioj;iojio;ijjiojijj;iooj8ioji;ojiio;jo;ijjioijo;;oji;oijjoi;oji",  7879890));
+	printf("Fake: %d\n", a);
+	printf("Real: %d\n", b);
 	return (0);
 }
 // Pour lancer, compiler dans l ordre
